ae11b5f00d248269510bbaf6ed306f966d95034e
Angelo981 <angeloseliaso90@gmail.com>
2023-02-03 22:35:20 +0200

    Update HTML server site

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import time
 import logging 
-#from Logs_extract import extract_incoming_logs
+import sys
+sys.path.append('C:\\Users\\ailias\\Desktop\\XM_project\\XM_Technical_Task')
+import Site_body as sb
 
 #HOST = "192.168.1.185"
 HOST = "localhost"
-PORT = 8080
-log_file = "C:\\Users\\ailias\\Desktop\\ServerIncoming_Sever_logs.txt"
+PORT = 9999
+#log_file = "ServerIncoming_Sever_logs.txt"
+#logging.basicConfig(filename='test.log',level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
+#logging.basicConfig(filename='test.log', level=logging.INFO)
 
 class NeuralHTTP(BaseHTTPRequestHandler):
     
     def do_GET(self):
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        self.wfile.write(bytes(sb.Html_site('HTML_Site.txt'), "utf-8"))
         
-        self.wfile.write(bytes("<html><body><h1> Hello World!</h1></body></html>", 
-                               "utf-8"))
-        logging.basicConfig(format='%(asctime)s - %(message)s!!!')
-        logging.info("This is a info message")
+    def do_GET2(self):
+
+        if ola_komple:
+            self.wfile.write(bytes("QIFSA", "utf-8"))
+            self.send_response(200)
+        else: 
+            self.send_response(404)
+            self.send_error({"Error":"Not found"})
+            
         
     def do_POST(self):
         self.send_response(200)
@@ -27,9 +38,10 @@ class NeuralHTTP(BaseHTTPRequestHandler):
         
         date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         self.wfile.write(bytes('{"time": "'+ date + '"}', "utf-8"))
-        #extract_incoming_logs(log_file)
+     
+                   
 
-        
+
         
 server = HTTPServer((HOST,PORT), NeuralHTTP)
 print("Server is now running")
